d35fb9c613a0b895be0bf31b9e341f3b47efa201
kmelct <kmelct@gmail.com>
2019-08-25 23:48:31 +0300

    Add security middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+// Core
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Middlewares
+import * as csurf from 'csurf';
+import * as helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add middlewares
+  app.use(helmet());
+  app.use(csurf());
+
   await app.listen(3000);
 }
 bootstrap();
