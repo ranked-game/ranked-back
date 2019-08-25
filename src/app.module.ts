cf26b203c20d01f6bd0956e431fed5e6cde240c9
kmelct <kmelct@gmail.com>
2019-08-25 23:39:07 +0300

    Init graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
+// Core
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+// Utils
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: false,
+      debug: true,
+      playground: true,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
