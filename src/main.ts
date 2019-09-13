9368fd1870de759220bc056a4e5db43a3975161e
kmelct <kmelct@gmail.com>
2019-09-14 02:05:55 +0300

    Fix csurf middleware

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,15 @@ import * as dotenv from 'dotenv';
 // Middlewares
 import * as csurf from 'csurf';
 import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Add middlewares
+  app.use(cookieParser());
   app.use(helmet());
-  app.use(csurf());
+  app.use(csurf({ cookie: true }));
   app.setGlobalPrefix('api');
 
   // Run server
