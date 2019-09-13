8c7ecf7978c5f17424f75fd546bf338223f1c077
kmelct <kmelct@gmail.com>
2019-09-14 01:36:46 +0300

    Add dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
+// Config
+import * as dotenv from 'dotenv';
+
 // Middlewares
 import * as csurf from 'csurf';
 import * as helmet from 'helmet';
@@ -11,11 +14,12 @@ async function bootstrap() {
 
   // Add middlewares
   app.use(helmet());
-  // app.use(csurf());
+  app.use(csurf());
   app.setGlobalPrefix('api');
 
   // Run server
   const port = process.env.PORT || 6001;
   await app.listen(port);
 }
+dotenv.config();
 bootstrap();
