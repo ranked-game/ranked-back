6f1a6966658d05cf7bae1c7ead264e14c4ffdd8a
kmelct <kmelct@gmail.com>
2019-09-27 19:52:52 +0300

    mail: impliment email compile logic

--- src/modules/mail/services/email.service.ts
+++ src/modules/mail/services/email.service.ts
@@ -3,6 +3,10 @@ import { Injectable, Logger } from '@nestjs/common';
 
 // Utils
 import * as nodemailer from 'nodemailer';
+import { readFile } from 'fs';
+import { promisify } from 'util';
+import * as path from 'path';
+import handlebars from 'handlebars';
 
 // Config
 import { mailSesTransportConfig } from '../config';
@@ -15,10 +19,13 @@ export class EmailService {
   );
 
   public async sendPromotionalEmail(receiver: string) {
+    const template = await this.compileEmailTemplate('mvpSubscriptionEmail');
+
     const mailOptions = {
       from: 'Ranked Game <mvp@ranked.game>',
       to: receiver,
       subject: 'Ranked Subscription',
+      html: template,
     };
 
     this.transporter.sendMail(mailOptions, (err, info) => {
@@ -28,5 +35,26 @@ export class EmailService {
         this.logger.log(info);
       }
     });
+  }
+
+  private async compileEmailTemplate(type: string) {
+    const readFileAsync = promisify(readFile);
+
+    const html = await readFileAsync(
+      path.join(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        '..',
+        'public',
+        'mvpSubscriptionEmail.html',
+      ),
+      { encoding: 'utf-8' },
+    );
+
+    const template = handlebars.compile(html);
+
+    return template({});
   }
 }
