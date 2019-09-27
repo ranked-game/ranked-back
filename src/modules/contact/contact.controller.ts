7a83698545fea55a9bf935528748bb8f96f572f5
kmelct <kmelct@gmail.com>
2019-09-27 20:08:21 +0300

    mail: add db save in mvp subscribe

--- src/modules/contact/contact.controller.ts
+++ src/modules/contact/contact.controller.ts
@@ -1,21 +1,36 @@
 // Core
-import { Controller, Post, Req, Get, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 // Services
 import { EmailService } from '../mail';
+import { UnitOfWorkService } from '../dbl';
 
 // Dto
 import { SubscribeMvpDto } from './dto';
 
+// Utils
+import { v4 as uuid } from 'uuid';
+
 @Controller('contact')
 export class ContactController {
-  constructor(private readonly emailService: EmailService) {}
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly unitOfWork: UnitOfWorkService,
+  ) {}
 
   @Post('mvp/subscribe')
   async subscribeMvp(@Body() subscriber: SubscribeMvpDto) {
     const { email } = subscriber;
 
-    await this.emailService.sendPromotionalEmail(email);
+    // Send email
+    // await this.emailService.sendPromotionalEmail(email);
+
+    // Save to db
+    const mvpRepository = await this.unitOfWork.getMvpSubscribersRepository();
+    const user = await mvpRepository.findOne({ email });
+    if (!user) {
+      await mvpRepository.insert({ id: uuid(), email });
+    }
 
     return { success: true };
   }
--- src/modules/contact/contact.module.ts
+++ src/modules/contact/contact.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 
 // Modules
 import { MailModule } from '../mail';
+import { DblModule } from '../dbl';
 
 // Controllers
 import { ContactController } from './contact.controller';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, DblModule],
   controllers: [ContactController],
 })
 export class ContactModule {}
